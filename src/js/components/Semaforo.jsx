8923af17d93533bd4f45847de6d14af87547e5a1
Caarlos3 <crt.981995@gmail.com>
2025-04-24 19:56:36 +0000

    Parte visual realizada

---
+++ src/js/components/Semaforo.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Semaforo = () => {
+
+    return(
+
+        <div className="container">
+            <div className="box">
+                <div className="lightRed">.</div>
+                <div className="lightYellow">.</div>
+                <div className="lightGreen">.</div>
+            </div>
+        </div>
+    )
+}
+
+export default Semaforo;
